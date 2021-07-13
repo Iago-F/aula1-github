497046ca38274ec092e142b6aa3a167c00f843fe
Iago França <iago_tony@hotmail.com>
2021-07-13 18:32:02 -0300

    Acrescentada mensagens de boa tarde

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,8 +4,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world! ");
-		System.out.println("Boa noite");
+		System.out.println("Bom dia! ");
+		System.out.println("Boa Tarde!");
 	}
 
 }
